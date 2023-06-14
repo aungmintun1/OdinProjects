75e9c8228a4e85c7395b0b798212a89a116cb297
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2023-06-13 23:11:01 -0400

    added javascript exercise files

---
+++ javascript-exercises/repeatString.js
@@ -0,0 +1,16 @@
+const repeatString = function(hey, number) {
+
+    let result = '';
+
+    for(var i = 0; i < number; i++){
+
+        result += hey;
+    
+    }
+
+    return result;
+
+};
+
+// Do not edit below this line
+module.exports = repeatString;
