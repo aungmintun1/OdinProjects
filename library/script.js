488acec1d227283a3a49acb49e604ede7775cae6
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2023-06-24 01:03:18 -0400

    made changes to form

--- library/script.js
+++ library/script.js
@@ -1,28 +1,7 @@
 
+const container = document.getElementById("container");
+let submitButton = document.getElementById('sub');  // variable connecting to HTML submit button
 let myLibrary = [];   // array of objects
-
-let titles=[];
-let authors=[];
-let pageList =[];
-
-// array containing object properties
-
-
-titleList.innerText= 'title: ' 
-authorList.innerText='author: ' 
-pageNum.innerText = 'pages: ' 
-
-//default header content
-
-let submitButton=document.getElementById('sub');  // variable connecting to HTML submit button
-//
-
-submitButton.addEventListener('click', function() {
-  addBookToLibrary();
-  display();
-});
-
-//adds an event listener to the submit button and when clicked it runs the following functions
 
 function Book(title,author,pages) {
   this.title = title;
@@ -32,26 +11,27 @@ function Book(title,author,pages) {
 }
 //the constructor for the book which has a title, author and page numbers
 
+submitButton.addEventListener('click', function() {
+  event.preventDefault();
+  addBookToLibrary();
+  createBox(book);
+});
+
+//adds an event listener to the submit button and when clicked it runs the following functions
+
+
+
 function addBookToLibrary() {
 
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
-
-
    
     var newBook = new Book(title,author,pages);
     myLibrary.push(newBook);
+    createBox(newBook);
 
-    titles.push(newBook.title);
-    authors.push(newBook.author);
-    pageList.push(newBook.pages);
-     
-      titleList.innerText= 'title: ' + titles;
-      authorList.innerText='author: ' + authors;
-      pageNum.innerText = 'pages: ' + pageList;
-  
-  
+
     /* 
     1. user types into the input and the strings get stored in their respective value
     2. with that data we input those variables into the constructor which makes a new book
@@ -60,5 +40,54 @@ function addBookToLibrary() {
     5. we alter the headers by adding in the content of the arrays into the innerText
 
     */
+    
+  
 }
+
+// Function to create a new div box
+function createBox(book) {
+
+  // Create a new div element
+  const box = document.createElement("div");
+
+  const header = document.createElement("h1");
+  const header2 = document.createElement("h1");
+  const header3 = document.createElement("h1");
+  
+  // Set the class and content of the box
+  box.className = "box";
+
+  header.textContent = 'title: ' + book.title;
+  header2.textContent = 'author: ' + book.author;
+  header3.textContent = 'pages: ' + book.pages;
+  
+  // Append the box to the container
+  box.appendChild(header);
+  box.appendChild(header2);
+  box.appendChild(header3);
+  container.appendChild(box);
+
+  formOverlay.classList.remove('active');
+
+}
+
+
+// const openFormButton = document.getElementById('openFormButton');
+
+const openFormButton = document.getElementById('addBtn');
+
+
+
+
+const formOverlay = document.getElementById('formOverlay');
+
+openFormButton.addEventListener('click', function() {
+  formOverlay.classList.add('active');
+});
+
+formOverlay.addEventListener('click', function(event) {
+  if (event.target === formOverlay) {
+    formOverlay.classList.remove('active');
+  }
+});
 
