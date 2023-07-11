2d15b07da18dbcecc83f63267d8977e30336c670
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2023-07-10 20:04:47 -0400

    added print js

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,27 @@
 
 import './App.css';
+import Print from './Print';
 
 function App() {
+
+  const planets = [
+    { name: "Mars", isGasPlanet: false },
+    { name: "Earth", isGasPlanet: false },
+    { name: "Jupiter", isGasPlanet: true},
+    { name: "Venus", isGasPlanet: false },
+    { name: "Neptune", isGasPlanet: true },
+    { name: "Uranus", isGasPlanet: true },
+    ];
+
   return (
     <div className="App">
-      hello world
+
+        {planets.map((planet) => planet.isGasPlanet && 
+        (
+        <Print PlanetName={planet.name}/>
+      ))}
+     
+    
     </div>
   );
 }
