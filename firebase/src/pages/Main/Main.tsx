68a103f420580f07a5cccbf550afe6c38f0e29c9
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2023-08-15 20:59:33 -0400

    modifying new features

--- firebase/src/pages/Main/Main.tsx
+++ firebase/src/pages/Main/Main.tsx
@@ -15,9 +15,11 @@ export interface Post {
 export default function Main() {
 
 
-  const postsRef = collection (db, "posts"); 
+  
   const [postsList, setPostsList] = useState<Post[] | null>(null);
 
+  const postsRef = collection (db, "posts"); 
+  
   const getPosts = async () => {
     const data = await getDocs(postsRef);
     setPostsList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[] );
--- firebase/src/pages/Main/Post.tsx
+++ firebase/src/pages/Main/Post.tsx
@@ -14,9 +14,10 @@ interface Like {
 }
 
 interface Comment{
-  userId: string;
-  likeId: string;
-  comment: string;
+  userId: string | '';
+  displayName: string;
+  postId: string;
+  comment: string | '';
 }
 
 export default function Post(props: Props) {
@@ -24,13 +25,13 @@ export default function Post(props: Props) {
     const {post} = props;
     const [user] = useAuthState(auth);
 
-    const[likeAmount, setLikeAmount] = useState<Like[] | null >(null)
+    const[likeAmount, setLikeAmount] = useState<Like[] | null >(null);
     const[commentText, setCommentText] = useState<string | null>("");
+    const[commentList, setCommentList] = useState<Comment[]>([]);
 
     const inputComment = (e : any) => {
       setCommentText(e.target.value)
-
-    }
+}
 
     const likesRef = collection(db, "likes");
 
@@ -81,30 +82,55 @@ export default function Post(props: Props) {
         }
         };
 
-        const commentRef = collection(db, "comments");
-
+        const commentsRef = collection(db, "comments");
         const addComment = async () => {
           try
-         { await addDoc(commentRef, {userId: user?.uid, postId: post.id, comment: commentText ,})
+         { 
+          if (user) {
+            const newCommentData = {
+              userId: user.uid,
+              displayName: user.displayName || '',
+              postId: post.id,
+              comment: commentText || '',
+            };
+      
+            await addDoc(commentsRef, newCommentData);
+      
+            setCommentList(prevComments => prevComments ? [...prevComments, newCommentData] : [newCommentData]);
 
-          // adds a doc to our collection in "likes"
-          // if(user){
-          // setLikeAmount((prev) => prev ? [...prev, { userId: user?.uid, likeId: newDoc.id}] : [{ userId: user?.uid ,likeId: newDoc.id}]);
-          // }
-        
-        }
-    
-          catch (err) {
-            console.log(err)
+            /*I had a typescript error. looks like I can't have the array as null, and i had the modify the comment string in my interface
+              a string or an empty string if it is null.
+              I also had to add that in the newCommentData object as well.
+
+              now when i activate the addComment(); it also sets the commentList array to the prev elements and adds the newCommentData object in it.
+
+            */
           }
+
+        } catch (err) {
+          console.log(err);
+        }
           };
+
+      const commentsDoc = query(commentsRef, where("postId" , "==", post.id)) 
+      const getComments = async () => {
+       const data = await getDocs(commentsDoc)
+
+       setCommentList(data.docs.map((doc) =>({
+        userId: doc.data().userId,
+        postId: doc.data().postId,
+        displayName: doc.data().displayName,
+        comment: doc.data().comment})));
+
+       
+      }
     
   
 
 
 useEffect(() => {
   getLikes();
-
+  getComments();
 }, []);
 
   return (
@@ -117,12 +143,27 @@ useEffect(() => {
        { likeAmount && <p>likes:{likeAmount?.length}</p>}
        
       <div className='commentSection'>
+
        <textarea className='comment-input' 
        placeholder="Write your comment..."
-       onChange={inputComment}
-       >
+        onChange={inputComment}>
        </textarea>
        <button onClick={addComment}>Submit Comment</button>
+       
+       <div className='commentList'>
+
+        {commentList?.map((comment) => 
+
+        <div className='commentBox'>
+
+           <p>posted by:{comment.displayName} : {comment.comment}</p>
+           <textarea placeholder='reply'></textarea>
+        </div>
+  
+        )}
+
+       </div>
+
        </div>
       
       
@@ -137,42 +178,57 @@ ADDING LIKES
 1. create the button
 2. we can add a like property in out posts object. however we wouldn't know who liked and set a limit to likes
 3. we first make a new collection "likes" in firebase
-4. we make a property {userId, postId}. everytime someone likes a post, the postID(ID of the post ) and userID (person who liked) 
+4. we make a property {userId, postId}. everytime someone likes a post, the postID(ID of the post from firebase not in the field) and userID (person who liked) 
   gets sent to the database.
 
+  b. we import the useAuthState() function to get information from the logged in user
+  const [user] = useAuthState(auth);
+
 5. we then add the likesRef. To reference the collection
-6. we then add a addLike() function in order to add a like to our collection.
+ const likesRef = collection(db, "likes");
+
+6. we then add an addLike() function in order to add a like to our collection.
 
    a. we declare it as an async function
    b. we use the addDoc() function to add a document to our collection. 
    we also reference what fields we want to have in the document that we will send to the database
 
-7. we import the useAuthState() function to get information from the logged in user
-  const [user] = useAuthState(auth);
+   at this point if you attach the function to the button onClick EL it will send data to the firebase collection
 
-8. add in the data for the documents. we get user.uid from auth and post id from the prop
+       const addLike = async () => {
+     await addDoc(likesRef, {userId: user?.uid, postId: post.id})
+      // adds a doc to our collection in "likes"
+      };
+
+7. add in the data for the documents. we get user.uid from auth and post id from the prop
+
 const addLike = async () => {
 await addDoc(likesRef, {userId: user?.uid, postId: post.id})
 
-9. add addLike() to the onclick EL of the button
+8. add addLike() to the onclick EL of the button
 
 HOW TO DISPLAY LIKES:
+
+The way we are going to display the likes is by gathering all the documents in "likes" that have the postId field = post.id (which is all the likes in the specific post)
+We will then find the number of those documents and use .length in order to set the state to that number.
+We then put the likeAmount state variable in the tag that we display in.
 
 1. import query from firestore
 import { addDoc, collection, query } from 'firebase/firestore';
 
-2. we must specify exactly what document we want. similar to likesRef
-we specify what collection we want. now its for the document.
+b. create a useState for the number of likes for each post and add a likes <p> tag to display
+ const[likeAmount, setLikeAmount] = useState<number | null >(null)
+{ likeAmount && <p>likes:{likeAmount}</p>}
+
+2. we must specify exactly what documents we want. similar to likesRef
+we specify what collection we want. now its for the documents. 
 
 const likesDoc = query(likesRef, where("postId" , "==", post.id))
 
 we reference the collection with likesRef. next we use the where() function to reference a specific document with a condition.
 In this condition, we are comparing the postId field in our DB and comparing it to the post.id that we are currently in.
+we only want the documents that match with the postId in our like field = post.id from our firebase DB
 
-b. create a useState for the number of likes for each post and add a likes <p> tag
- const[likeAmount, setLikeAmount] = useState<number | null >(null)
-{ likeAmount && <p>likes:{likeAmount}</p>}
-   
 
 3. create getLikes();  //sets the amount of likes in the state and then is used to display the like
 
@@ -290,6 +346,66 @@ const addLike = async () => {
 /*
 HOW TO CREATE COMMENT
 
-make a collection called comments. field should have userID, postId, comment
+1.make a collection called comments. field should have userID, postId, comment
 
+b.make 2 states. one is an array of the string comments, and one is for setting the string value of a new comment that will be stored in the state array.
+  You must also make an interface for the array that stores objects, list the properties in the object and their type.
+   const[commentText, setCommentText] = useState<string | null>("");
+   const[commentList, setCommentList] = useState<Comment[] | null>(null);
+
+  interface Comment{
+  userId: string;
+  postId: string;
+  comment: string;
+  }
+
+  c. Create a function that takes the input of the user and puts it in the commentText state. add the onChange EL to the textarea.
+         const inputComment = (e : any) => {
+        setCommentText(e.target.value)
+        }
+
+2.make a function that adds a document to our "comments" collection. add the function to the onClick EL to the submit button.
+
+       const commentsRef = collection(db, "comments");
+        const addComment = async () => {
+          try
+         { await addDoc(commentsRef, {userId: user?.uid, postId: post.id, comment: commentText,}) 
+       
+        }
+          catch (err) {
+            console.log(err)
+          }
+          };
+
+      3.display the comments. 
+
+      first we reference a condition which retrieves specific documents where the condition is postId from field is = to post.id of firebase DB
+
+      now we make our getComments(); function:
+      we get all the documents that satisfy the condition and it is stored in "data" which is an array of the docs.
+      then we add all those documents to our commentList state array by mapping.
+       each time we loop we make a new object with the properties. we get them by using doc.data()."fieldName"
+
+      const commentsDoc = query(commentsRef, where("postId" , "==", post.id)) 
+      const getComments = async () => {
+       const data = await getDocs(commentsDoc)
+
+       setCommentList(data.docs.map((doc) =>({
+        userId: doc.data().userId,
+        postId: doc.data().postId,
+        comment: doc.data().comment})));
+       
+      }
+    
+      4. we add getComments(); to our useEffect hook
+
+      5. we map through all the objects in our commentList array and use that information to create a p tag that contains the comment string.
+       {commentList?.map((comment) => 
+
+        <div className='commentBox'>
+           <p>{comment.comment}</p>
+           <textarea placeholder='reply'></textarea>
+        </div>
+  
+        )}
 */
