cde4e088787acd6f5c1dc849e88f5d777806cfb8
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2023-08-23 00:14:45 -0400

    adding quantity feature in cart

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -4,25 +4,45 @@ import { useState } from 'react';
 function App() {
 
   const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [quantity, setQuantity] = useState(1);
 
+  const tShirt = { price: 20, name: "t-shirt" , quantity: quantity}
+  const tank = {price: 30, name: "tank"}
+  const pants = {price: 35, name: "pants" }
+
+  const addCart = (item) => {
+    setTotal(total + item.price)
+    setCart([...cart , item])
+  }
 
   return (
     <div className="App">
 
       <div>
+      
+      <p>total:${total}</p>
+
+      {cart.map((item) =>
+       <p>{item.name} ${item.price} x{item.quantity} </p>
+      )}
+      </div>
+      
+
+      <div>
           <div>
-            <p> T-shirt $20</p>
-            <button>buy</button>
+            <p> T-shirt ${tShirt.price}</p>
+            <button onClick={() => addCart(tShirt)}>buy</button>
           </div>
 
           <div>
-            <p> tank-top $30</p>
-            <button>buy</button>
+            <p> tank-top ${tank.price}</p>
+            <button onClick={() => addCart(tank)}>buy</button>
           </div>
 
           <div>
-            <p> pants $35</p>
-            <button>buy</button>
+            <p> pants ${pants.price}</p>
+            <button onClick={() => addCart(pants)}>buy</button>
           </div>
 
       </div>
