7dd1416ea535f60000a5b0ba6fe45d4dee70f319
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2023-08-27 00:50:10 -0400

    modified and simplified dispatch functions

--- reactRedux/redux/src/pages/Counter.js
+++ reactRedux/redux/src/pages/Counter.js
@@ -7,24 +7,26 @@ export default function Counter() {
    
     const dispatch = useDispatch();
     const myNum = useSelector((state) => state.counter.value);
+
+    const addition = (num) =>{
+      dispatch(add(num))
+    }
+
+    const subtraction = (num) =>{
+      dispatch(subtract(num))
+    }
+
+    const setZero = (num) => {
+      dispatch(zero(num))
+    }
     
   return (
     <div>
         <h1>this is the Counter Page</h1>
         <h2>number: {myNum} </h2>
-        <button onClick={()=> dispatch(add())}>+1</button>
-        <button onClick={()=> dispatch(subtract())}>-1</button>
-        <button onClick={()=> dispatch(zero())}>set to zero</button>
-
-        {
-        /*
-        This is the original code
-         const [num, setNum] = useState(0)
-         <button onClick={()=> setNum(num+1)}>+1</button>
-        <button onClick={()=> setNum(num-1)}>-1</button>
-        <button onClick={()=> setNum(0)}>set to 0</button> */
-        }
-        
+        <button onClick={()=> addition(myNum)}>+1</button>
+        <button onClick={()=> subtraction(myNum)}>-1</button>
+        <button onClick={()=> setZero(myNum)}>set to zero</button>
 
     </div>
   )
