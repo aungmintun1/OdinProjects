682e7f512b64e899ebf16ae235da9f3fd3539ff6
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2023-10-14 18:50:51 -0400

    changed images to components and made image array of images that will be sent as props to image components

--- image-slider/src/App.js
+++ image-slider/src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import ImageSlider from './ImageSlider';
+import Scroll from './Scroll';
 
+import image1 from './pic1.jpg';
+import image2 from './pic2.jpg';
+import image3 from './pic3.jpg';
 
 function App() {
   
+const imgArray =[image1,image2,image3,image1,image2,image3]
   
   return (
     <div className="App">
 
-      <ImageSlider />
+      {/* <ImageSlider /> */}
+
+      {imgArray.map((el) => (
+        <Scroll pic={el} />
+      ))}
       
-    
+   {/* <Scroll pic={image1}/> */}
+  
+
     </div>
   );
 }
--- image-slider/src/Scroll.js
+++ image-slider/src/Scroll.js
@@ -1,24 +1,56 @@
 import React from 'react'
 import "./fade.css"
-import Fade from "react-reveal/Fade"
 
-export default function Scroll() {
+import { useState, useRef, useEffect} from 'react';
+
+export default function Scroll(props) {
+
+  const [view, setView] = useState(false);
+  const img = useRef(null);
+
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                setView(true);
+            } else {
+                setView(false);
+            }
+        });
+    });
+
+    if (img.current) {
+        observer.observe(img.current);
+    }
+
+    // Cleanup: Unobserve the target when component unmounts
+    return () => {
+        if (img.current) {
+            observer.unobserve(img.current);
+        }
+    };
+}, []); // Empty dependency array means this useEffect runs once after component mounts
+
   return (
     <>
+
 <div className="container">
-<Fade top> 
 
-<div className="card"></div>
-<div className="card"></div>
-<div className="card"></div>
-<div className="card"></div>
-<div className="card"></div>
-<div className="card"></div>
-<div className="card"></div>
-<div className="card"></div>
-<div className="card"></div>
 
-</Fade>
+    <img ref={img} className={`card ${view ? "fade" : ""}`} src={props.pic} alt="" />
+
+
+    {/* <img ref ={img} className ={`card ${view ? "fade" : ""}`} src={image1} alt="" />
+    <img ref ={img} className ={`card ${view ? "fade" : ""}`} src={image2} alt="" />
+    <img ref ={img} className ={`card ${view ? "fade" : ""}`} src={image3} alt="" />
+    <img className = "card" src={image1} alt="" />
+    <img className = "card" src={image2} alt="" />
+    <img className = "card" src={image3} alt="" />
+    <img className = "card" src={image1} alt="" />
+    <img className = "card" src={image2} alt="" />
+    <img className = "card" src={image3} alt="" /> */}
+
 </div>
     
     </>
@@ -27,6 +59,6 @@ export default function Scroll() {
 
 // 1. npm i react-reveal --save --legacy-peer-deps
 
-// 2. wrap the Fade tag around the divs that you want to have teh fade with
+// 2. wrap the Fade tag around the divs that you want to have the fade with
 
 // 3. read documenation that deals with how you can modify the fade
